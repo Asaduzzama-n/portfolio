2f2421034753bfef19101a8563ac0e743078d442
Asaduzzaman <masaduzzaman193146@bscse.uiu.ac.bd>
2024-12-06 19:25:49 +0600

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <SpeedInsights />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SpeedInsights />
+
+        {children}
+      </body>
     </html>
   );
 }
